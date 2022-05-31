025cc2434192a3fcb9df38770ec1680b76ea7cdd
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2022-05-31 09:52:58 +0200

    Version 1.5.0-alpha2

--- lib/libmongocrypt_helper/version.rb
+++ lib/libmongocrypt_helper/version.rb
@@ -1,5 +1,5 @@
 module LibmongocryptHelper
-  VERSION = '1.5.0.alpha1.0.1001'
+  VERSION = '1.5.0.alpha2.0.1001'
 
-  LIBMONGOCRYPT_VERSION = '1.5.0-alpha1'
+  LIBMONGOCRYPT_VERSION = '1.5.0-alpha2'
 end
